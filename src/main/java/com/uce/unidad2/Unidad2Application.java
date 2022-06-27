2e437fb3deb0e9592c92966cfe54b0ab86bbd3fd
driven <aldazpaulo@gmail.com>
2022-06-26 20:14:32 -0500

    log4j

--- src/main/java/com/uce/unidad2/Unidad2Application.java
+++ src/main/java/com/uce/unidad2/Unidad2Application.java
@@ -1,11 +1,19 @@
 package com.uce.unidad2;
 
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class Unidad2Application implements CommandLineRunner{
+
+	private static Logger logger =  LogManager.getLogger(Unidad2Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Unidad2Application.class, args);
@@ -13,8 +21,15 @@ public class Unidad2Application implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola Mundo");
 		
+		System.out.println("Sin Logger");
+
+		logger.debug("It is a debug logger.");
+		logger.error("It is an error logger.");
+		logger.fatal("It is a fatal logger.");
+		logger.info("It is a info logger.");
+		logger.trace("It is a trace logger.");
+		logger.warn("It is a warn logger.");
 	}
 
 }
